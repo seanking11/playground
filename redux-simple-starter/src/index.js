92ce81535ee0bac1fe604bd123b3a9b1d246600c
Sean King <kingsc@miamioh.edu>
2018-01-16 17:12:41 -0600

    feat(App): Add App structure

--- redux-simple-starter/src/index.js
+++ redux-simple-starter/src/index.js
@@ -1,15 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import _ from 'lodash'
+import YTSearch from 'youtube-api-search'
+import apiKey from './apiKey.js'
+import SearchBar from './components/SearchBar'
+import VideoList from './components/VideoList'
+import VideoDetail from './components/VideoDetail'
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY = apiKey
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      videos: [],
+      selectedVideo: ''
+    }
+
+    this.videoSearch('Human Fall Flat')
+  }
+
+  videoSearch = term => {
+    YTSearch({ key: API_KEY, term }, videos => {
+      this.setState({ videos, selectedVideo: videos[0] })
+    })
+  }
+
+  render() {
+    const videoSearch = _.debounce(term => { this.videoSearch(term) }, 300)
+
+    return (
+      <div>
+        <SearchBar onSearchTermChange={videoSearch} />
+        <VideoDetail
+          video={this.state.selectedVideo}
+        />
+        <VideoList
+          videos={this.state.videos}
+          onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
+        />
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'))
