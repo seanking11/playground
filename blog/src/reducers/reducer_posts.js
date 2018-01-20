eda8eee3b130dc55d2c217d44ff53e924c758b3a
Sean King <kingsc@miamioh.edu>
2018-01-20 17:44:51 -0600

    feat(Actions): Add createPost action and implement in reducer

--- blog/src/reducers/reducer_posts.js
+++ blog/src/reducers/reducer_posts.js
@@ -1,9 +1,11 @@
 import _ from 'lodash'
-import { FETCH_POSTS } from '../actions'
+import { FETCH_POSTS, CREATE_POST } from '../actions'
 
 export default function (state = {}, action) {
   switch (action.type) {
     case FETCH_POSTS:
+      return _.mapKeys(action.payload.data, 'id')
+    case CREATE_POST:
       return _.mapKeys(action.payload.data, 'id')
     default:
       return state
