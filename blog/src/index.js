900e9e59a5f37260c574bbd345ba7c2d66c0677f
Sean King <kingsc@miamioh.edu>
2018-01-20 16:22:11 -0600

    feat(Routing): Add routing configuration

--- blog/src/index.js
+++ blog/src/index.js
@@ -1,15 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import promise from 'redux-promise'
+import reducers from './reducers'
+import PostsIndex from './components/PostsIndex'
+import PostsNew from './components/PostsNew'
 
-import App from './components/app';
-import reducers from './reducers';
-
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore)
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/' component={PostsIndex} />
+        <Route path='/posts/new' component={PostsNew} />
+      </Switch>
+    </BrowserRouter>
   </Provider>
-  , document.querySelector('.container'));
+  , document.querySelector('.container')
+)
