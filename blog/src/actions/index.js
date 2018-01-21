65aba3681016f10a8bd842baa4f22138a713c308
Sean King <kingsc@miamioh.edu>
2018-01-21 12:46:14 -0600

    feat(Posts): Add delete post functionality

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -4,6 +4,7 @@ import axios from 'axios'
 export const FETCH_POSTS = 'FETCH_POSTS'
 export const CREATE_POST = 'CREATE_POST'
 export const FETCH_POST = 'FETCH_POST'
+export const DELETE_POST = 'DELETE_POST'
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api'
 const API_KEY = '?key=BANANAGRAMZ'
@@ -33,5 +34,15 @@ export const fetchPost = (id) => {
   return {
     type: FETCH_POST,
     payload: request
+  }
+}
+
+export const deletePost = (id, callback) => {
+  const request = axios.delete(`${ROOT_URL}/posts/${id}${API_KEY}`)
+    .then(() => callback())
+
+  return {
+    type: DELETE_POST,
+    payload: id
   }
 }
