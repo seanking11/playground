be00761b87183e7b94455478b88616eaf2917b84
Sean King <kingsc@miamioh.edu>
2018-01-20 20:40:34 -0600

    feat(Posts): Add navigation to individual posts

--- blog/src/components/PostsIndex.js
+++ blog/src/components/PostsIndex.js
@@ -12,7 +12,9 @@ class PostsIndex extends Component {
   renderPosts = () => (
     _.map(this.props.posts, post => (
       <li className='list-group-item' key={post.id}>
-        {post.title}
+        <Link to={`/posts/${post.id}`}>
+          {post.title}
+        </Link>
       </li>
     ))
   )
