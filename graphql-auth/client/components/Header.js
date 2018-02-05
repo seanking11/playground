2fbb45a71fd7651c2d89ab6f1717fe8e91ed75a8
Sean King <kingsc@miamioh.edu>
2018-02-05 17:47:50 -0500

    feat(Apollo): Send cookies with requests

--- graphql-auth/client/components/Header.js
+++ graphql-auth/client/components/Header.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react'
+import { graphql } from 'react-apollo'
+import query from '../queries/CurrentUser'
 
 class Header extends Component {
+  componentWillMount() {
+
+  }
+
   render() {
+    console.log(this.props);
     return (
       <div>Header</div>
     )
   }
 }
 
-export default Header
+export default graphql(query)(Header)
