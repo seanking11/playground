11ff8cf59b050dc88a60d951591b30aaa66db607
Sean King <kingsc@miamioh.edu>
2018-02-06 00:07:01 -0500

    feat(Header): Add logout functionality

--- graphql-auth/client/components/Header.js
+++ graphql-auth/client/components/Header.js
@@ -1,18 +1,56 @@
 import React, { Component } from 'react'
-import { graphql } from 'react-apollo'
+import { graphql, compose } from 'react-apollo'
+import { Link } from 'react-router'
 import query from '../queries/CurrentUser'
+import mutation from '../mutations/Logout'
 
 class Header extends Component {
-  componentWillMount() {
+  onLogoutClick() {
+    this.props.mutate({
+      refetchQueries: [{ query }]
+    })
+  }
 
+  renderButtons() {
+    const { loading, user } = this.props.data
+
+    if (loading) { return <div /> }
+
+    if (user) {
+      return (
+        <li>
+          <a onClick={this.onLogoutClick.bind(this)}>Logout</a>
+        </li>
+      )
+    } else {
+      return (
+        <div>
+          <li>
+            <Link to='/signup'>Signup</Link>
+          </li>
+          <li>
+            <Link to='/login'>Login</Link>
+          </li>
+        </div>
+      )
+    }
   }
 
   render() {
-    console.log(this.props);
     return (
-      <div>Header</div>
+      <nav>
+        <div className='nav-wrapper'>
+          <Link to='/' className='brand-logo left'>Home</Link>
+          <ul className='right'>
+            {this.renderButtons()}
+          </ul>
+        </div>
+      </nav>
     )
   }
 }
 
-export default graphql(query)(Header)
+export default compose(
+  graphql(mutation),
+  graphql(query)
+)(Header)
