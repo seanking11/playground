fb1e7775e32556d3660e331608fe11b9549ba3b0
Sean King <kingsc@miamioh.edu>
2018-02-06 00:35:12 -0500

    feat(LoginForm): Add LoginForm and login functionality

--- graphql-auth/client/components/App.js
+++ graphql-auth/client/components/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Header from './Header'
 
 export default (props) => (
-  <div>
+  <div className='container'>
     <Header />
     {props.children}
   </div>
---
+++ graphql-auth/client/components/LoginForm.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import { graphql } from 'react-apollo'
+import AuthForm from './AuthForm'
+import mutation from '../mutations/Login'
+import query from '../queries/CurrentUser'
+
+class LoginForm extends Component {
+  onSubmit({ email, password }) {
+    this.props.mutate({
+      variables: { email, password },
+      refetchQueries: [{ query }]
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Login</h3>
+        <AuthForm
+          onSubmit={this.onSubmit.bind(this)}
+        />
+      </div>
+    )
+  }
+}
+
+export default graphql(mutation)(LoginForm)
