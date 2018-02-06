553a8f5c6b5ecda1caea5536a27a2f5baaf8e697
Sean King <kingsc@miamioh.edu>
2018-02-06 00:51:29 -0500

    feat(AuthForm): Add error handling

--- graphql-auth/client/components/LoginForm.js
+++ graphql-auth/client/components/LoginForm.js
@@ -5,10 +5,15 @@ import mutation from '../mutations/Login'
 import query from '../queries/CurrentUser'
 
 class LoginForm extends Component {
+  state = { errors: [] }
+
   onSubmit({ email, password }) {
     this.props.mutate({
       variables: { email, password },
       refetchQueries: [{ query }]
+    }).catch(err => {
+      const errors = err.graphQLErrors.map(error => error.message)
+      this.setState({ errors })
     })
   }
 
@@ -18,6 +23,7 @@ class LoginForm extends Component {
         <h3>Login</h3>
         <AuthForm
           onSubmit={this.onSubmit.bind(this)}
+          errors={this.state.errors}
         />
       </div>
     )
