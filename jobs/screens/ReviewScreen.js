c5b3ceba5cd2064f2b166fe6539fd3954db1eaef
Sean King <kingsc@miamioh.edu>
2018-02-09 21:06:22 -0500

    feat(ReviewScreen): Add stack navigation configuration

--- jobs/screens/ReviewScreen.js
+++ jobs/screens/ReviewScreen.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react'
 import { View, Text } from 'react-native'
 
 class ReviewScreen extends Component {
+  static navigationOptions = {
+    title: 'Review Jobs',
+    headerRight: (
+      <Text>Right</Text>
+    )
+  }
+
   render() {
     return (
       <View>
