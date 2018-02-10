cafe8f57a895a60f22ccdec7894fd184a8f2f20c
Sean King <kingsc@miamioh.edu>
2018-02-09 21:13:18 -0500

    feat(ReviewScreen): Add styling and programmatic navigation

--- jobs/screens/ReviewScreen.js
+++ jobs/screens/ReviewScreen.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react'
 import { View, Text } from 'react-native'
+import { Button } from 'react-native-elements'
 
 class ReviewScreen extends Component {
-  static navigationOptions = {
+  static navigationOptions = ({ navigation }) => ({
     title: 'Review Jobs',
     headerRight: (
-      <Text>Right</Text>
+      <Button
+        title='Settings'
+        onPress={() => { navigation.navigate('settings') }}
+        backgroundColor='rgba(0, 0, 0, 0)'
+        color='rgba(0, 122, 255, 1)'
+      />
     )
-  }
+  })
 
   render() {
     return (
