a3474676651194101299d490d63699ad3a0fb4c6
Sean King <kingsc@miamioh.edu>
2019-01-30 14:49:42 -0600

    Add "I am rich" app to udemy-swift

---
+++ udemy-swift/I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Sean King on 1/28/19.
+//  Copyright © 2019 Sean King. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
